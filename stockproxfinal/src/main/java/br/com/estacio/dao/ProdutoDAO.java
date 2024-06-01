e41beb4ba6f3d6695c9b9df5f657841c8ba39b1b
EmersonAF <169107753+EmersonAF@users.noreply.github.com>
2024-06-01 17:12:07 -0300

    Update ProdutoDAO.java

--- stockproxfinal/src/main/java/br/com/estacio/dao/ProdutoDAO.java
+++ stockproxfinal/src/main/java/br/com/estacio/dao/ProdutoDAO.java
@@ -2,31 +2,45 @@ package br.com.estacio.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import br.com.estacio.conexao.Conexao;
+import br.com.estacio.modelo.Estoque;
 import br.com.estacio.modelo.Produto;
 
 public class ProdutoDAO {
 	
-	public void cadastrarProduto (Produto produto) {
+	public static int cadastrarProduto(Produto produto) {
+		
+		int codigo = 0;
 		
 		String sql = "INSERT INTO PRODUTOS (NOME) VALUES (?)";
+		String sql2 =  "SELECT LAST_INSERT_ID()";
 				
 		PreparedStatement ps = null;
 		Connection con = Conexao.getConexao();
 		
 		try {
 			ps = con.prepareStatement(sql);
+			PreparedStatement ps2 = con.prepareStatement(sql2);
 			ps.setString(1, produto.getNome());
 			if(ps.executeUpdate()>0) {
 				System.out.println("CADASTRADO COM SUCESSO!!!");
 			}
+			
+			ResultSet rs = ps2.executeQuery(sql2);
+			while(rs.next()) {
+				codigo = rs.getInt(1);
+			}
+			
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			System.out.println(e.getMessage());
 		}
+		return codigo;
 	}
 	
 	public void alterarProduto(String nome, int id) {
@@ -74,5 +88,4 @@ public class ProdutoDAO {
 			}
 		}
 	}
-
 }
