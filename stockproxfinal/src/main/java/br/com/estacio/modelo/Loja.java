0a702abcab91a3e4473cfa49e827969f310d763e
EmersonAF <169107753+EmersonAF@users.noreply.github.com>
2024-06-01 17:14:01 -0300

    Update Loja.java

--- stockproxfinal/src/main/java/br/com/estacio/modelo/Loja.java
+++ stockproxfinal/src/main/java/br/com/estacio/modelo/Loja.java
@@ -1,5 +1,37 @@
 package br.com.estacio.modelo;
 
 public class Loja {
+	
+	private int id;
+	private String nome;
+	
+	@Override
+	public String toString() {
+		return "Loja [id=" + id + ", nome=" + nome + "]";
+	}
 
+	public Loja(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	public Loja(int id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return this.nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
 }
